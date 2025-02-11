a74ba4abc96678723ee95ee022a564e89017b34c
Jon Sagara <jonsagara@gmail.com>
2025-02-11 11:43:22 -0800

    Initial commit.

---
+++ src/ForLoopCompileTest/Program.cs
@@ -0,0 +1,34 @@
+﻿var length = 2;
+
+// This for loop no longer compiles with recent versions of roslyn, even though the
+//   code file from which it's extracted hasn't changed in 5 years:
+for (int offset = 0, c1, c2; offset < length;)
+{
+    // snipped
+}
+
+// If there's only one extra, uninitialized variable declaration, it compiles:
+for (int offset = 0, c1; offset < length;)
+{
+    // snipped
+}
+
+// When you add two or more uninitialized variable declarations, it does not compile:
+for (int offset = 0, c1, c2, c3; offset < length;)
+{
+    // snipped
+}
+
+// Workaround: initialize the aforementioned uninitialized variables:
+for (int offset = 0, c1 = 0, c2; offset < length;)
+{
+    // snipped
+}
+
+// -OR-
+
+for (int offset = 0, c1 = 0, c2 = 0; offset < length;)
+{
+    // snipped
+}
+
